132803f2bddad9f82c8aa566cf48c2432bae2110
정유진 <jeong-yujin@jeong-yujin-ui-MacBookAir.local>
2024-03-13 17:55:18 +0900

    nbcamp-folder move

---
+++ NumberBaseball/NumberBaseball/BaseballGame.swift
@@ -0,0 +1,19 @@
+//
+//  BaseballGame.swift
+//  NumberBaseball
+//
+//  Created by 정유진 on 2024/03/13.
+//
+
+import Foundation
+
+class BaseballGame {
+    func start() {
+        let answer = makeAnswer() // 정답을 만드는 함수
+    }
+    
+    func makeAnswer() -> Int {
+        // 함수 내부를 구현하기
+        return 0
+    }
+}
---
+++ NumberBaseball/NumberBaseball/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  NumberBaseball
+//
+//  Swift 문법 심화 과제 - 숫자 야구 게임
+//
+
+import Foundation
+
+/*
+ Lv1
+ - 1에서 9까지의 서로 다른 임의의 수 3개를 정하고 맞추는 게임입니다
+ - 정답은 랜덤으로 만듭니다.(1에서 9까지의 서로 다른 임의의 수 3자리)
+ */
+
+// BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+let game = BaseballGame()
+game.start()
