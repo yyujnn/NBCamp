1add731076ceec1c62fcf52d25c8f569ef480a6a
정유진 <jeong-yujin@jeong-yujin-ui-MacBookAir.local>
2024-03-13 20:12:40 +0900

    Lv1.baseball

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -7,12 +7,6 @@
 
 import Foundation
 
-/*
- Lv1
- - 1에서 9까지의 서로 다른 임의의 수 3개를 정하고 맞추는 게임입니다
- - 정답은 랜덤으로 만듭니다.(1에서 9까지의 서로 다른 임의의 수 3자리)
- */
-
-// BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+// Lv1. BaseballGame 인스턴스를 만들고 start 함수를 구현하기
 let game = BaseballGame()
 game.start()
